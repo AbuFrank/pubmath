568deef13122715d0bb539a3b79216f1753aef24
abufrank <riley.thornton1@gmail.com>
2020-11-06 22:28:41 -0500

    authcontext and rerouting

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import gql from "graphql-tag";
 import { useMutation } from "@apollo/react-hooks";
 import clsx from "clsx";
 
+import { AuthContext } from "../context/auth";
+
 import { useForm } from "../util/hooks";
 
 function Register(props) {
+  const context = useContext(AuthContext);
   const [errors, setErrors] = useState({});
 
   const { onChange, onSubmit, values } = useForm(registerUser, {
@@ -16,7 +19,8 @@ function Register(props) {
   });
 
   const [addUser, { loading }] = useMutation(REGISTER_USER, {
-    update(_, result) {
+    update(_, { data: { register: userData } }) {
+      context.login(userData);
       props.history.push("/");
     },
     onError(err) {
